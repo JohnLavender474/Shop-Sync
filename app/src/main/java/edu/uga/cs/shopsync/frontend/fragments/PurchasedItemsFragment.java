4520d52c79c3a468046fe54d6b8fbf28cad62085
John Lavender <82977289+JohnLavender474@users.noreply.github.com>
2023-11-27 17:06:29 -0500

    Design purchased item row xml

--- app/src/main/java/edu/uga/cs/shopsync/frontend/fragments/PurchasedItemsFragment.java
+++ app/src/main/java/edu/uga/cs/shopsync/frontend/fragments/PurchasedItemsFragment.java
@@ -1,9 +1,12 @@
 package edu.uga.cs.shopsync.frontend.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,35 +19,112 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import edu.uga.cs.shopsync.R;
 import edu.uga.cs.shopsync.backend.models.PurchasedItemModel;
+import edu.uga.cs.shopsync.frontend.Constants;
+import edu.uga.cs.shopsync.utils.ArraySetList;
+import edu.uga.cs.shopsync.utils.CallbackReceiver;
+import edu.uga.cs.shopsync.utils.Props;
 
 public class PurchasedItemsFragment extends Fragment implements ChildEventListener {
+
+    private static final String TAG = "PurchasedItemsFragment";
+
+    public static final String ACTION_INITIALIZE_PURCHASED_ITEMS =
+            "ACTION_INITIALIZE_PURCHASED_ITEMS";
+    public static final String ACTION_UNDO_PURCHASE = "ACTION_UNDO_PURCHASE";
+    public static final String ACTION_DELETE_PURCHASE = "ACTION_DELETE_PURCHASE";
+    public static final String ACTION_EDIT_COST_PER_UNIT = "ACTION_EDIT_COST_PER_UNIT";
+    public static final String ACTION_EDIT_QUANTITY = "ACTION_EDIT_QUANTITY";
+    public static final String PROP_PURCHASED_ITEMS = "PROP_PURCHASED_ITEMS";
+
+    private final List<PurchasedItemModel> purchasedItems;
+    private final PurchasedItemsAdapter adapter;
+
+    private CallbackReceiver callbackReceiver;
+
+    public PurchasedItemsFragment() {
+        super();
+
+        Log.d(TAG, "PurchasedItemsFragment: called");
+
+        callbackReceiver = null;
+        purchasedItems = new ArraySetList<>();
+        adapter = new PurchasedItemsAdapter(purchasedItems);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView: called");
+
+        // if the activity does not implement CallbackReceiver, then throw an exception
+        // otherwise, set the callbackReceiver to the activity
+        if (callbackReceiver == null) {
+            if (!(getActivity() instanceof CallbackReceiver)) {
+                Log.e(TAG, "onCreateView: Activity must implement CallbackReceiver");
+                throw new ClassCastException("Activity must implement CallbackReceiver");
+            }
+
+            Log.d(TAG, "onCreateView: Activity implements CallbackReceiver");
+            callbackReceiver = (CallbackReceiver) getActivity();
+        }
+
         View view = inflater.inflate(R.layout.fragment_purchased_items, container, false);
+
+        // set up the RecyclerView and its adapter
         RecyclerView recyclerView = view.findViewById(R.id.recyclerViewPurchasedItems);
-
-        // TODO: Initialize purchased items (replace this with your actual data retrieval)
-        List<PurchasedItemModel> purchasedItems = new ArrayList<>();
-
-        // Set up the RecyclerView and its adapter
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        PurchasedItemsAdapter adapter = new PurchasedItemsAdapter(purchasedItems);
         recyclerView.setAdapter(adapter);
+
+        callbackReceiver.onCallback(ACTION_INITIALIZE_PURCHASED_ITEMS, Props.of(
+                Pair.create(PROP_PURCHASED_ITEMS, purchasedItems),
+                Pair.create(Constants.ADAPTER, adapter)));
 
         return view;
     }
 
     @Override
-    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+    public void onStart() {
+        Log.d(TAG, "onStart: called");
+        super.onStart();
 
+        if (!(getActivity() instanceof CallbackReceiver)) {
+            Log.e(TAG, "onStart: Activity must implement CallbackReceiver");
+            throw new ClassCastException("Activity must implement CallbackReceiver");
+        }
+
+        Log.d(TAG, "onStart: Activity implements CallbackReceiver");
+        callbackReceiver = (CallbackReceiver) getActivity();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+
+        Log.d(TAG, "onDestroy: setting callbackReceiver to null");
+        callbackReceiver = null;
+    }
+
+    @Override
+    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+        PurchasedItemModel purchasedItem = snapshot.getValue(PurchasedItemModel.class);
+        Log.d(TAG, "onChildAdded: purchasedItem = " + purchasedItem);
+
+        if (purchasedItem == null) {
+            Log.e(TAG, "onChildAdded: purchasedItem is null");
+            return;
+        }
+
+        // TODO: very inefficient to add to beginning of array list, a better way to do this
+        purchasedItems.add(0, purchasedItem);
+
+
+        adapter.notifyItemInserted(0);
     }
 
     @Override
@@ -67,7 +147,7 @@ public class PurchasedItemsFragment extends Fragment implements ChildEventListen
 
     }
 
-    private static class PurchasedItemsAdapter
+    private class PurchasedItemsAdapter
             extends RecyclerView.Adapter<PurchasedItemsAdapter.ViewHolder> {
 
         private final List<PurchasedItemModel> purchasedItems;
@@ -80,7 +160,7 @@ public class PurchasedItemsFragment extends Fragment implements ChildEventListen
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.row_purchase_group, parent, false);
+                    .inflate(R.layout.row_purchased_item, parent, false);
             return new ViewHolder(view);
         }
 
@@ -94,17 +174,21 @@ public class PurchasedItemsFragment extends Fragment implements ChildEventListen
             return purchasedItems.size();
         }
 
-        private static class ViewHolder extends RecyclerView.ViewHolder {
+        private class ViewHolder extends RecyclerView.ViewHolder {
 
-            private final TextView textViewUsername;
+            private final TextView textViewEmail;
             private final TextView textViewTotalCost;
-            private final TextView textViewTimeOfPurchase;
+            private final Button buttonUndoPurchase;
+            private final Button buttonDeletePurchase;
 
             ViewHolder(@NonNull View itemView) {
                 super(itemView);
+                // TODO: fix
+                /*
                 textViewUsername = itemView.findViewById(R.id.textViewUsername);
                 textViewTotalCost = itemView.findViewById(R.id.textViewTotalCost);
                 textViewTimeOfPurchase = itemView.findViewById(R.id.textViewTimeOfPurchase);
+                 */
             }
 
             void bind(PurchasedItemModel purchasedItem) {
